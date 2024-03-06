d07bcf1568e2cfc1f46daa909450ae617119c206
userbyte <42632711+userbyte@users.noreply.github.com>
2024-03-06 01:08:00 -0500

    Squashed commit of the following:
    commit da84d20a98274f8988864d13ae586f4e6eccfda9
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Wed Apr 12 22:57:19 2023 -0400
    
        fghgfh
    
    commit 1debf6659fa810af1e1e1e25a9be6093dd13c2d2
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Wed Apr 12 22:52:32 2023 -0400
    
        sdfsdfsdf
    
    commit c849a4f874f3c8eec79086c0f5b5afdf3d7b3755
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Mon Apr 10 16:26:13 2023 -0400
    
        dont do version check
    
    commit e0764ddbfb028d9079b4a0d73264c379cc773a5c
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Mon Apr 10 00:55:13 2023 -0400
    
        Update README.md
    
    commit 35e132eabe3aef6a5c79684bff95fee83691afa3
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Mon Apr 10 00:46:47 2023 -0400
    
        w
    
    commit 97ad43cacb1f38cd2b328b579a99bbb77d96b063
    Author: userbyte <42632711+userbyte@users.noreply.github.com>
    Date:   Mon Apr 10 00:28:39 2023 -0400
    
        add playpause command

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -72,7 +72,9 @@ public class JMusicBot
         Prompt prompt = new Prompt("JMusicBot");
         
         // startup checks
-        OtherUtil.checkVersion(prompt);
+
+        // fuck a version check idc
+        // OtherUtil.checkVersion(prompt);
         OtherUtil.checkJavaVersion(prompt);
         
         // load config
@@ -122,6 +124,7 @@ public class JMusicBot
                         new ForceskipCmd(bot),
                         new MoveTrackCmd(bot),
                         new PauseCmd(bot),
+                        new PlayPauseCmd(bot),
                         new PlaynextCmd(bot),
                         new RepeatCmd(bot),
                         new SkiptoCmd(bot),
--- src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
+++ src/main/java/com/jagrosh/jmusicbot/utils/OtherUtil.java
@@ -158,17 +158,18 @@ public class OtherUtil
     }
     
     public static void checkVersion(Prompt prompt)
-    {
+    {   
+        return;
         // Get current version number
-        String version = getCurrentVersion();
+        // String version = getCurrentVersion();
         
-        // Check for new version
-        String latestVersion = getLatestVersion();
+        // // Check for new version
+        // String latestVersion = getLatestVersion();
         
-        if(latestVersion!=null && !latestVersion.equals(version))
-        {
-            prompt.alert(Prompt.Level.WARNING, "JMusicBot Version", String.format(NEW_VERSION_AVAILABLE, version, latestVersion));
-        }
+        // if(latestVersion!=null && !latestVersion.equals(version))
+        // {
+        //     prompt.alert(Prompt.Level.WARNING, "JMusicBot Version", String.format(NEW_VERSION_AVAILABLE, version, latestVersion));
+        // }
     }
     
     public static String getCurrentVersion()
@@ -184,7 +185,7 @@ public class OtherUtil
         try
         {
             Response response = new OkHttpClient.Builder().build()
-                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/jagrosh/MusicBot/releases/latest").build())
+                    .newCall(new Request.Builder().get().url("https://api.github.com/repos/userbyte/better-jmusicbot/releases/latest").build())
                     .execute();
             ResponseBody body = response.body();
             if(body != null)
